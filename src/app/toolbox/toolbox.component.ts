7a12132ba7fba1e2f4de9be08451600c3e736457
dmitryRomankov <strmr@tut.by>
2018-10-23 14:14:40 +0300

    toolbox component

--- src/app/toolbox/toolbox.component.ts
+++ src/app/toolbox/toolbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-toolbox',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbox.component.css']
 })
 export class ToolboxComponent implements OnInit {
+  public faPlusSquare = faPlusSquare;
 
   public ngOnInit(): void { }
 
